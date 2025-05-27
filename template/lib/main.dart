518c7fb6263e5c647b3499e3152f3f4e3f3d0e4b
Erkan Öztürk <43139925+erkanozturk1903@users.noreply.github.com>
2025-05-27 20:30:41 +0300

    🎯 Add main.dart with app initialization

---
+++ template/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+import 'core/di/injection_container.dart' as di;
+import 'core/theme/app_theme.dart';
+import 'features/home/presentation/pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize dependencies
+  await di.init();
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Master Template',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
